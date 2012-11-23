3a80ccf79b47cbf42b61483435f51912ed4e9c19
Uri Klar <uriklar@gmail.com>
2012-11-23 19:51:12 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Openfinance::Application.config.secret_token = '005d4b2ca1248c5d2412a0d1d63e2af02f75440dbee7f372277b1bf61995d0a3fe01fb6065e660389b9f6b7f8e58b81cee58b0809331feeefd1b07cc8faefc1c'
