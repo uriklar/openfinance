b4a68b5cd3107867c4dea0e62d7594a9e53e8d4d
Gal Ben Shlomo <benshlomogal@gmail.com>
2013-02-15 22:16:33 +0200

    bubbles reading json

--- app/controllers/transfer_controller.rb
+++ app/controllers/transfer_controller.rb
@@ -1,11 +1,21 @@
 class TransferController < ApplicationController
 	def index
 		#@transfers = Transfer.select("transfer_id,section_id,sum(net) as net").group("transfer_id,section_id").having("sum(net) <> ?",0).order("transfer_id ASC")
-	end
+		@transfers = Transfer.select("section_id,section_name,sum(net) as net").group("section_id,section_name").order("section_id ASC")
+		
+		a = []
 
-	def flare
-		respond_to do |format|
-    		format.json { render :json => { 1 => "First", 2 => "Second"} }
-    	end
+		@transfers.each do |section| 
+			h = Hash.new()
+			h["name"] = section.section_id
+			h["size"] = section.net.abs
+			a.push(h)
+		end
+
+		sectionhash = Hash.new()
+		sectionhash["name"] = "stringush"
+		sectionhash["children"] = a
+		@jsonex = sectionhash.to_json
+
 	end
 end
